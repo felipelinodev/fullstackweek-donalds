4a49bffd17f1f09afad60edc38ed311a458d8f8b
felipelinodev <felipelinodesigndoe@gmail.com>
2025-10-11 19:26:00 -0300

    fix: user is redirected with cpf after order creation

--- donalds/src/app/[slug]/menu/actions/create-order.ts
+++ donalds/src/app/[slug]/menu/actions/create-order.ts
@@ -60,5 +60,7 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
-  redirect(`/${input.slug}/orders`);
+  redirect(
+    `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.costumerCpf)}`,
+  );
 };

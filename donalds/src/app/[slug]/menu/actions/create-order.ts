72a38bd47d21ed7f2f3d9f062942f78d20669705
felipelinodev <felipelinodesigndoe@gmail.com>
2025-10-11 21:49:34 -0300

    feat: ensure cash is revalidate when creating an order

--- donalds/src/app/[slug]/menu/actions/create-order.ts
+++ donalds/src/app/[slug]/menu/actions/create-order.ts
@@ -1,5 +1,6 @@
 "use server";
 import { ConsumptionMethod } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -60,6 +61,7 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  revalidatePath(`/${input.slug}/orders`);
   redirect(
     `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.costumerCpf)}`,
   );
